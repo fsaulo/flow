87be6ca8522ad01f5e94ba1ddf7e9af87fad79e4
Saulo G. Felix <sgf3lix@gmail.com>
2023-06-26 21:41:35 -0300

    Add dummy code

---
+++ src/flow.h
@@ -0,0 +1,14 @@
+#ifndef _FLOW_H
+#define _FLOW_H
+
+#include <chrono>
+#include <thread>
+
+#define PI 3.14159265358979323846
+
+#ifdef USING_OPENCV
+#include <opencv2/videoio.hpp>
+#include <opencv2/device.hpp>
+#endif
+
+#endif // _FLOW_H
